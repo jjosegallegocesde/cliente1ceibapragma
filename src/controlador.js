5f0b727515b23267c806d17b54becd54f51fbe3d
dcm323 <danielcalle323@gmail.com>
2023-04-27 12:01:48 -0500

    se pintan canciones

--- src/controlador.js
+++ src/controlador.js
@@ -1,11 +1,36 @@
 import {pedirTokenEnSpotify} from '../services/servicioSpotify.js'
 import {buscarCanciones} from '../services/servicioConsultarCanciones.js'
 
+let fila=document.getElementById("fila")
+
+
 pedirTokenEnSpotify()
 .then(function(token){
     
     buscarCanciones(token)
     .then(function(respuesta){
+
+        respuesta.tracks.forEach(function(cancion){
+
+            let columna=document.createElement("div")
+            columna.classList.add("col")
+
+            let tarjeta=document.createElement("div")
+            tarjeta.classList.add("card","h-100","shadow")
+
+            let pista=document.createElement("audio")
+            pista.classList.add("w-100")
+            pista.setAttribute("controls","controls")
+            pista.src=cancion.preview_url
+
+            tarjeta.appendChild(pista)
+            columna.appendChild(tarjeta)
+            fila.appendChild(columna)
+
+
+
+        })
+
         console.log(respuesta.tracks[0])
         console.log(respuesta.tracks[0].name)
         console.log(respuesta.tracks[0].preview_url)
